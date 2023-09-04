e9e253bcb8204183fa7157dceef602bc354fc245
Guy Bedford <gbedford@fastly.com>
2023-09-04 16:52:14 -0700

    latest jco

--- shell/lib/exports/wasi-io-streams.d.ts
+++ shell/lib/exports/wasi-io-streams.d.ts
@@ -1,10 +1,10 @@
 export namespace WasiIoStreams {
-  export function read(this: InputStream, len: bigint): [Uint8Array, StreamStatus];
-  export function blockingRead(this: InputStream, len: bigint): [Uint8Array, StreamStatus];
-  export function dropInputStream(this: InputStream): void;
-  export function write(this: OutputStream, buf: Uint8Array | ArrayBuffer): [bigint, StreamStatus];
-  export function blockingWrite(this: OutputStream, buf: Uint8Array | ArrayBuffer): [bigint, StreamStatus];
-  export function dropOutputStream(this: OutputStream): void;
+  export function read(this_: InputStream, len: bigint): [Uint8Array, StreamStatus];
+  export function blockingRead(this_: InputStream, len: bigint): [Uint8Array, StreamStatus];
+  export function dropInputStream(this_: InputStream): void;
+  export function write(this_: OutputStream, buf: Uint8Array | ArrayBuffer): [bigint, StreamStatus];
+  export function blockingWrite(this_: OutputStream, buf: Uint8Array | ArrayBuffer): [bigint, StreamStatus];
+  export function dropOutputStream(this_: OutputStream): void;
 }
 export type InputStream = number;
 /**
